c0524843dcd5120f9347edfb199d7b5eaca3864b
unknown <tilsa.bobadilla@gmail.com>
2025-01-12 14:32:11 -0800

    hueles a pedo

---
+++ js/juego.js
@@ -0,0 +1,19 @@
+var vida, ataque,pocion;
+var nombre, textoBienvenide;
+
+nombre="Kira"
+textoBienvenide= "Hola";
+
+vida=100;
+ataque=10;
+pocion=20;
+
+vida++;
+
+vida -= ataque;
+vida += pocion;
+
+
+
+console.log(textoBienvenide + " " + nombre);
+console.log(vida);
