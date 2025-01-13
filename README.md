6e6a40f26953a4cc8d422f401cb2f7841a377003
unknown <tilsa.bobadilla@gmail.com>
2025-01-13 07:42:27 -0800

    dia dos

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Este es mi primer repo para JavaScript
 - ´git add .´ para agregar todos los cambios
 - ´git commit -m "el mensaje que quieras"´ para hacer commit de los cambios
 - ´git push´ para subir los cambios
+
--- js/juego.js
+++ js/juego.js
@@ -1,19 +1,42 @@
-var vida, ataque,pocion;
+var vida, ataque, pocion;
 var nombre, textoBienvenide;
 
-nombre="Kira"
-textoBienvenide= "Hola";
+nombre = "Kira";
+textoBienvenide = "Hola";
 
-vida=100;
-ataque=10;
-pocion=20;
+vida = 100;
+ataque = 10;
+pocion = 20;
 
-vida++;
+function ataqueEnemigo() {
+  vida -= ataque;
 
-vida -= ataque;
-vida += pocion;
+  if (vida <= 0) {
+    console.log("Game Over");
+  } else {
+    muestraVida();
+}
+  }
+
+function tomarPocion() {
+    vida += pocion;
+    console.log(vida);
+    if (vida => 100) {
+        console.log("Vida al maximo");
+    } else {
+      muestraVida();
+    }
+}
+
+function muestraVida() {
+  console.log("Nivel de vida del heroe");
+  console.log(vida);
+}
+  
+
+
 
 
 
 console.log(textoBienvenide + " " + nombre);
-console.log(vida);
+// console.log(vida);
