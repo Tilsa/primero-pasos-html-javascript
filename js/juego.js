7f282f1875383ce36702fd8cf4374a159ddd5f48
unknown <tilsa.bobadilla@gmail.com>
2025-01-14 15:30:40 -0800

    dia tres

--- js/juego.js
+++ js/juego.js
@@ -1,5 +1,6 @@
-var vida, ataque, pocion;
+var vida, Ataque, pocion;
 var nombre, textoBienvenide;
+var numero_1, numero_2, resultado;
 
 nombre = "Kira";
 textoBienvenide = "Hola";
@@ -8,9 +9,12 @@ vida = 100;
 ataque = 10;
 pocion = 20;
 
-function ataqueEnemigo() {
+function ataqueEnemigo(potencia, nombreAtaque) {
   vida -= ataque;
-
+  vida -= potencia;
+  console.log("El enemigo ha atacado con " + nombreAtaque);
+  console.log("Vida restante: " + vida);
+  muestraVida(nombreAtaque);
   if (vida <= 0) {
     console.log("Game Over");
   } else {
@@ -21,21 +25,25 @@ function ataqueEnemigo() {
 function tomarPocion() {
     vida += pocion;
     console.log(vida);
-    if (vida => 100) {
-        console.log("Vida al maximo");
-    } else {
-      muestraVida();
-    }
 }
 
 function muestraVida() {
   console.log("Nivel de vida del heroe");
   console.log(vida);
+  
 }
   
+function accion(){
+  numero1 = prompt('Ingrese un numero');
+  numero2 = prompt('Ingrese otro numero');
+  resultado = suma(numero1, numero2);
+}
 
-
-
+function suma(num1, num2){
+ var valor;
+ valor = num1 + num2;
+ return (valor);
+}
 
 
 console.log(textoBienvenide + " " + nombre);
