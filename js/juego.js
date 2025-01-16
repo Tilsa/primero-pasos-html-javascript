bfb73285b3fc47080afa29c8db267193906887bd
unknown <tilsa.bobadilla@gmail.com>
2025-01-16 13:49:59 -0800

    dia cinco

--- js/juego.js
+++ js/juego.js
@@ -1,4 +1,8 @@
 var vida, ataque, pocion;
+var Pocion = 0
+var colaFenix = 1
+var pocionquemaduras = 2
+var Veneno = 3
 var nombre, textoBienvenide;
 var numero_1, numero_2, resultado;
 var turno = 1;
@@ -6,7 +10,7 @@ var atacar = 0;
 var quemar = 1;
 var envenenar = 2;
 var fallar = 3;
-var vida = 100;
+vida = 100;
 
 //ESTADOS DEL JUGADOR
 var vivo = true;
@@ -61,12 +65,20 @@ function suma(num1, num2) {
 function JuegaTurno() {
   var jugadaCPU = Math.floor(Math.random() * 4);
 
-  if (vida > 0) {
-    console.log("Jugada CPU: " + jugadaCPU);
+  if(vida <= 0){
+    vivo = false;
+  }
+
+  if(vida <= 0){
+    vivo = false;
+  }
+
+  if (vivo == true) {
+    console.log("Jugada CPU");
 
     if (jugadaCPU == atacar) {
       vida -= 10;
-      console.log("He atacado all jugador");
+      console.log("He atacado al jugador");
     }
     if (jugadaCPU == quemar) {
       console.log("He quemado al jugador");
@@ -79,13 +91,56 @@ function JuegaTurno() {
       console.log("He fallado");
     }
     muestraEstadoJugador();
+  } else {
+    console.log("Game Over");
   }
 }
 
 function muestraEstadoJugador() {
-  console.log("Vida: " + vida);
-  if (envenenado == true) console.log("Usted esta envenenado");
-  if (quemado == true) console.log("Usted esta quemado");
+  if(vivo == true){
+    console.log("Vida: " + vida);
+    if(envenenado == true) 
+      console.log("Usted esta envenenado");
+    if(quemado == true)
+      console.log("Usted esta quemado");
+  } else {
+    console.log("Game Over");
+  }
+}
+function usarItem(objeto) {
+
+if(objeto == pocion){
+  vida += 50;
+}
+if(objeto == colaFenix){
+  if(vivo == false) {
+    vivo = true;
+    vida = 30;
+  } else {
+    console.log("No puedes usar este item");
+  }
+}
+if(objeto == pocionquemaduras){
+  if(quemado == true)
+    quemado = false;
+
+  else {
+   console.log("No puedes usar este item");
+  }
+}
+if(objeto == Veneno){
+  if (envenenado == true)
+  envenenado = false;
+  else {
+    console.log("No puedes usar este item");
+  }
+
 }
 
-console.log(textoBienvenide + " " + nombre);
+  muestraEstadoJugador();
+
+
+
+        console.log(textoBienvenide + " " + nombre);
+}
+  
