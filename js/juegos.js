527387ae6293649af9f87084c742c8bc05548b6f
unknown <tilsa.bobadilla@gmail.com>
2025-01-19 18:19:25 -0500

    no se que poner estoy aburrida

---
+++ js/juegos.js
@@ -0,0 +1,10 @@
+var configTeclado = { prevent_repeat : true};
+var eventoTeclado = new window.keypress.Listener(this,configTeclado);
+
+
+function pulsaA(){
+    console.log("Has pulsado a");
+}
+
+eventoTeclado.simple_combo("a", pulsaA);
+
